439f582840c9ba9b79465367cc002073d74fc759
Jean Jacobi <jjacobi@student.42.fr>
2016-12-14 18:42:09 +0100

    Working

--- srcs/stock_infos.c
+++ srcs/stock_infos.c
@@ -6,7 +6,7 @@
 /*   By: jjacobi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/14 14:07:29 by jjacobi           #+#    #+#             */
-/*   Updated: 2016/12/14 15:23:59 by jjacobi          ###   ########.fr       */
+/*   Updated: 2016/12/14 18:41:41 by jjacobi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ static t_infos	*create_infos_struct(void)
 	infos.precision = -1;
 	infos.lenght_modifs[0] = '\0';
 	infos.lenght_modifs[1] = '\0';
+
 	return (infos);
 }
 
@@ -42,18 +43,34 @@ t_infos		stock_infos(char *str)
 	i = 0;
 	while (str[i] && !ft_strchr("sSpdDioOuUxXcC%", str[i]))
 	{
-		if (ft_isdigit(str[i]))
+		if (ft_isdigit(str[i]) && str[i] != '0')
 		{
-			if (str[i] == '0')
-				infos.flag_zero = 1;
 			tmp = ft_custom_atoi(&str[i], &i);
 			if (str[i++] == '$')
-			{
-				infos.argx = tmp;
-				infos.flag_zero = 0;
-			}
+				infos->argx = tmp;
 			else
-				infos.minimum_size = tmp;
+				infos->minimum_size = tmp;
 		}
+		else if (str[i] == '0' && i++)
+			infos.flag_zero = 1;
+		else if (str[i] == '#' && i++)
+			infos->flag_metadata = 1;
+		else if (str[i] == '-' && i++)
+			infos->flag_minus = 1;
+		else if (str[i] == '+' && i++)
+			infos->flag_plus = 1;
+		else if (str[i] == ' ' && i++)
+			infos->flag_space = 1;
+		else if (str[i] == '.' && i++)
+			infos->precision = ft_custom_atoi(&str[i], &i);
+		else if (str[i] == 'h' || str[i] == 'l' || str[i] == 'j' || str[i] == 'z')
+		{
+			infos.lenght_modifs[0] = str[i++];
+			if (str[i] == 'h' || str[i] == 'l')
+				infos.lenght_modifs[1] = str[i++];
+		}
+		else
+			return (NULL);
 	}
+	return (infos);
 }
