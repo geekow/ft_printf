02e46c4b3faa6f3cc9c1857783de960e1aa353e4
Jean Jacobi <jjacobi@student.42.fr>
2017-01-13 04:04:04 +0100

    Alpha release

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jjacobi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 15:04:04 by jjacobi           #+#    #+#             */
-/*   Updated: 2017/01/12 07:05:08 by jjacobi          ###   ########.fr       */
+/*   Updated: 2017/01/13 04:01:03 by jjacobi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,45 +16,28 @@
 int			ft_printf(const char *format, ...)
 {
 	va_list	args;
-	int		result;
-	size_t	last_write;
+	size_t	last_w;
 	size_t	i;
-	int		verif;
 	t_info	*info;
 
 	i = 0;
-	result = 0;
-	last_write = 0;
+	last_w = 0;
 	va_start(args, format);
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
-			if (i > last_write)
-			{
-				verif = addchars(&format[last_write], i - last_write);
-				if (verif == -1)
-					return (-1);
-				result += verif;
-			}
-			if (!(info = stock_info(format + ++i, &i)))
+			if (i > last_w && -1 == addchars(&format[last_w], i - last_w))
 				return (-1);
-			last_write = i;
-			verif = parse_info(info, args);
-			if (verif == -1)
+			if (!(info = stock_info(format + ++i, &i)) ||
+					-1 == parse_info(info, args))
 				return (-1);
-			else
-				result += verif;
+			last_w = i;
 		}
 		i++;
 	}
-	if (i > last_write)
-	{
-		verif = addchars(&format[last_write], i - last_write);
-		if (verif == -1)
-			return (-1);
-		result += verif;
-	}
+	if (i > last_w && -1 == addchars(&format[last_w], i - last_w))
+		return (-1);
 	va_end(args);
 	return (write_or_stock_all(NULL, 0, 1));
 }
