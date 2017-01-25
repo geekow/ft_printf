058e8057422eb70fd445999217c8fd731cfa7117
Jean Jacobi <jjacobi@student.42.fr>
2017-01-25 19:04:13 +0100

    Parsing d now working (No norm)

--- srcs/parse_d.c
+++ srcs/parse_d.c
@@ -6,68 +6,60 @@
 /*   By: jjacobi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/11 01:42:23 by jjacobi           #+#    #+#             */
-/*   Updated: 2017/01/23 21:27:03 by jjacobi          ###   ########.fr       */
+/*   Updated: 2017/01/25 19:01:05 by jjacobi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-/**
-int			parse_d(t_info *info, va_list args)
-{
-	int		d;
-	int		len;
-	char	*str;
-
-	d = (int)va_arg(args, int);
-	str = ft_itoa(d);
-	len = ft_strlen(str);
-	if (info->flag_space && d >= 0)
-		addchar(' ', 1);
-
-	if (info->min_size && info->precision > len)
-		addchar(' ', info->min_size - info->precision - info->flag_plus - info->flag_space);
-	else if ((info->min_size && (!info->flag_zero || info->precision != -1 )))
-		addchar(' ', info->min_size - info->flag_plus - len - info->flag_space);
-	if (d < 0)
-	{
-		addchar('-', 1);
-		len--;
-		str++;
-	}
-	if (info->flag_zero && info->precision == -1)
-		addchar('0', info->min_size - len - 1);
-	if (info->precision >= 0)
-	{
-		addchar('0', info->precision - len);
-	}
-	if (!addchars(str, len))
-		return (-1);
-	return (0);
-}**/
 
 int			parse_d(t_info *info, va_list args)
 {
 	int		d;
-	int		tmp;
 	char	*str;
+	int		size;
+	int		special;
 
 	d = (int)va_arg(args, int);
 	str = ft_itoa(d);
-	tmp = 0;
-	if (d > 0 && info->flag_space)
-		tmp++;
-	if (info->min_size > info->precision && !info->flag_zero)
+	special = 0;
+	if (info->precision != -1 || info->flag_minus)
 	{
-		if (info->precision > 0)
-			addchar(' ', info->min_size - (info->precision - ft_strlen(str)) - tmp);
+		if ((int)ft_strlen(str) < info->precision)
+			size = info->precision;
 		else
-			addchar(' ', info->min_size - ft_strlen(str) - tmp);
+			size = ft_strlen(str);
+		if ((info->flag_plus || info->flag_space) && d >= 0)
+			special++;
+		if (size < info->min_size && !info->flag_minus)
+			if (!addchar(' ', info->min_size - size - special))
+				return (-1);
+		if (info->flag_space && d >= 0 && !info->flag_plus)
+			addchar(' ', 1);
+		if (info->flag_plus && d >= 0)
+			addchar('+', 1);
+		if (info->precision >= 0)
+			addchar('0', size - ft_strlen(str));
+		if (info->precision == 0 && d == 0)
+			addchar(' ', 1);
+		else
+			addchars(str, ft_strlen(str));
+		if (info->flag_minus && (size + special) < info->min_size)
+			addchar(' ', info->min_size - (size + special));
+		return (0);
 	}
-	else if (info->min_size > info->precision && info->flag_zero)
+	else
 	{
-		addchar('0', info->min_size - ft_strlen(str) - tmp);
+		if ((info->flag_plus || info->flag_space) && d >= 0)
+			special++;
+		if (info->min_size > (special + (int)ft_strlen(str)) && !info->flag_zero)
+			addchar(' ', info->min_size - special - ft_strlen(str));
+		else if (info->min_size > (special + (int)ft_strlen(str)) && info->flag_zero)
+			addchar('0', info->min_size - special - ft_strlen(str));
+		if (info->flag_space && d >= 0 && !info->flag_plus)
+			addchar(' ', 1);
+		if (info->flag_plus && d >= 0)
+			addchar('+', 1);
+		addchars(str, ft_strlen(str));
 	}
-	if (!addchars(str, ft_strlen(str)))
-		return (-1);
 	return (0);
 }
