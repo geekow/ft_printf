72abc3e30bd1a95479f1d3c4ce41d367f20dbb96
Jean Jacobi <jjacobi@student.42.fr>
2017-01-26 22:17:53 +0100

    Version 1

--- srcs/parse_d.c
+++ srcs/parse_d.c
@@ -6,7 +6,7 @@
 /*   By: jjacobi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/11 01:42:23 by jjacobi           #+#    #+#             */
-/*   Updated: 2017/01/26 20:22:01 by jjacobi          ###   ########.fr       */
+/*   Updated: 2017/01/26 22:16:29 by jjacobi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,88 +32,107 @@ static long long	get_data(char modifs[2], va_list args)
 		return ((int)va_arg(args, int));
 }
 
+static int			treatment_without_zero_begining(t_info *info, int *special,
+														char **str, long long d)
+{
+	if ((info->flag_plus || info->flag_space) && d >= 0)
+		*special = *special + 1;
+	if (info->min_size > (*special + (int)ft_strlen(*str))
+		&& !info->flag_zero)
+	{
+		if (-1 == addchar(' ', info->min_size - *special - ft_strlen(*str)))
+			return (-1);
+	}
+	else if (info->min_size > (*special + (int)ft_strlen(*str))
+			&& info->flag_zero)
+	{
+		if (info->flag_plus && d >= 0)
+		{
+			if (-1 == addchar('+', 1))
+				return (-1);
+		}
+		else if (d < 0 && (*str = *str + 1) && (*special += 1))
+		{
+			if (-1 == addchar('-', 1))
+				return (-1);
+		}
+		if (-1 == addchar('0', info->min_size - *special - ft_strlen(*str)))
+			return (-1);
+	}
+	return (1);
+}
+
+static int			trt_one(t_info *info, char *str,
+													int *size, long long d)
+{
+	size[1] = 0;
+	if ((int)ft_strlen(str) < info->precision)
+		size[0] = info->precision;
+	else
+		size[0] = ft_strlen(str);
+	if ((info->flag_plus || info->flag_space) && d >= 0)
+		size[1] = size[1] + 1;
+	if (d < 0 && info->precision >= 0)
+		size[1] = size[1] + 1;
+	if (size[0] < info->min_size && !info->flag_minus)
+		if (!addchar(' ', info->min_size - size[0] - size[1]))
+			return (-1);
+	if (info->flag_space && d >= 0 && !info->flag_plus)
+		if (-1 == addchar(' ', 1))
+			return (-1);
+	if (info->flag_plus && d >= 0)
+		if (-1 == addchar('+', 1))
+			return (-1);
+	return (1);
+}
+
+static int			trt_two(t_info *info, int d,
+													char **str, int *size)
+{
+	if (info->precision >= 0)
+	{
+		if (d < 0 && (*str = *str + 1))
+		{
+			if (size[0] != info->precision)
+				size[0] = size[0] - 1;
+			if (addchar('-', 1) == -1 ||
+					addchar('0', size[0] - ft_strlen(*str)) == -1)
+				return (-1);
+		}
+		else if (-1 == addchar('0', size[0] - ft_strlen(*str)))
+			return (-1);
+	}
+	if (info->precision == 0 && d == 0 && info->min_size != -1)
+	{
+		if (addchar(' ', 1) == -1)
+			return (-1);
+	}
+	else if (info->precision != 0 || d != 0)
+		if (-1 == addchars(*str, ft_strlen(*str)))
+			return (-1);
+	if (info->flag_minus && (size[0] + size[1]) < info->min_size)
+		if (-1 == addchar(' ', info->min_size - (size[0] + size[1])))
+			return (-1);
+	return (1);
+}
+
 int					parse_d(t_info *info, va_list args)
 {
 	long long	d;
 	char		*str;
-	int			size;
-	int			special;
+	int			size[2];
 
 	d = get_data(info->lenght_modifs, args);
 	str = ft_itoa(d);
-	special = 0;
 	if (info->precision != -1 || info->flag_minus)
 	{
-		if ((int)ft_strlen(str) < info->precision)
-			size = info->precision;
-		else
-			size = ft_strlen(str);
-		if ((info->flag_plus || info->flag_space) && d >= 0)
-			special++;
-		if (d < 0 && info->precision >= 0)
-			special++;
-		if (size < info->min_size && !info->flag_minus)
-			if (!addchar(' ', info->min_size - size - special))
-				return (-1);
-		if (info->flag_space && d >= 0 && !info->flag_plus)
-			if (-1 == addchar(' ', 1))
-				return (-1);
-		if (info->flag_plus && d >= 0)
-			if (-1 == addchar('+', 1))
-				return (-1);
-		if (info->precision >= 0)
-		{
-			if (d < 0)
-			{
-				str++;
-				if (size != info->precision)
-					size--;
-				if (addchar('-', 1) == -1 || addchar('0', size - ft_strlen(str)) == -1)
-					return (-1);
-			}
-			else if (-1 == addchar('0', size - ft_strlen(str)))
-				return (-1);
-		}
-		if (info->precision == 0 && d == 0 && info->min_size != -1)
-		{
-			if (addchar(' ', 1) == -1)
-				return (-1);
-		}
-		else if (info->precision != 0 || d != 0)
-			if (-1 == addchars(str, ft_strlen(str)))
-				return (-1);
-		if (info->flag_minus && (size + special) < info->min_size)
-			if (-1 == addchar(' ', info->min_size - (size + special)))
-				return (-1);
+		if (!trt_one(info, str, size, d) || !trt_two(info, d, &str, size))
+			return (-1);
 	}
 	else
 	{
-		if ((info->flag_plus || info->flag_space) && d >= 0)
-			special++;
-		if (info->min_size > (special + (int)ft_strlen(str))
-				&& !info->flag_zero)
-		{
-			if (-1 == addchar(' ', info->min_size - special - ft_strlen(str)))
-				return (-1);
-		}
-		else if (info->min_size > (special + (int)ft_strlen(str))
-				&& info->flag_zero)
-		{
-			if (info->flag_plus && d >= 0)
-			{
-				if (-1 == addchar('+', 1))
-					return (-1);
-			}
-			else if (d < 0)
-			{
-				str++;
-				special++;
-				if (-1 == addchar('-', 1))
-					return (-1);
-			}
-			if (-1 == addchar('0', info->min_size - special - ft_strlen(str)))
-				return (-1);
-		}
+		if (-1 == treatment_without_zero_begining(info, &size[1], &str, d))
+			return (-1);
 		if (info->flag_space && d >= 0 && !info->flag_plus)
 			if (-1 == addchar(' ', 1))
 				return (-1);
