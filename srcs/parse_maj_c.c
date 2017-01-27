7fc1de1a0fe6bd16991160cce082a1550f25291e
Jean Jacobi <jjacobi@student.42.fr>
2017-01-27 18:19:30 +0100

    Fix bug

--- srcs/parse_maj_c.c
+++ srcs/parse_maj_c.c
@@ -6,7 +6,7 @@
 /*   By: jjacobi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/11 01:42:34 by jjacobi           #+#    #+#             */
-/*   Updated: 2017/01/13 06:17:30 by jjacobi          ###   ########.fr       */
+/*   Updated: 2017/01/27 18:18:17 by jjacobi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 int	parse_maj_c(t_info *info, va_list args)
 {
 	info->lenght_modifs[0] = 'l';
+	info->lenght_modifs[1] = '\0';
 	return (parse_c(info, args));
 }
