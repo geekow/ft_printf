4629e0e519adeacf236c1ab438aae3e45ec9b19c
Jean Jacobi <jjacobi@student.42.fr>
2017-01-29 18:39:00 +0100

    Final touch

--- srcs/parse_s.c
+++ srcs/parse_s.c
@@ -6,16 +6,82 @@
 /*   By: jjacobi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/11 01:43:06 by jjacobi           #+#    #+#             */
-/*   Updated: 2017/01/11 05:07:20 by jjacobi          ###   ########.fr       */
+/*   Updated: 2017/01/29 17:00:12 by jjacobi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include "libft.h"
+#include <wchar.h>
 
-int	parse_s(t_info *info, va_list args)
+static int	flag_zero(char flag_minus, char flag_zero, int min_size, int i)
 {
-	if (info && args)
-		return (0);
+	if (!flag_minus && flag_zero && ((min_size - i) > 0))
+		return (addchar('0', min_size - i));
+	return (1);
+}
+
+static int	flag_space(char flag_minus, char flag_zero, int min_size, int i)
+{
+	if (!flag_minus && !flag_zero && ((min_size - i) > 0))
+		return (addchar(' ', min_size - i));
+	return (1);
+}
+
+static int	get_and_save(t_info *info, wchar_t **fws, char **fs, va_list args)
+{
+	int			i;
+	wchar_t		*wstr;
+	char		*str;
+
+	i = 0;
+	if (info->lenght_modifs[0] == 'l' && info->lenght_modifs[1] == '\0')
+	{
+		wstr = (wchar_t*)va_arg(args, wchar_t*);
+		if (!wstr)
+			wstr = (wchar_t*)ft_strdup("(null)");
+		while (wstr[i])
+			i = i + 1;
+		*fws = wstr;
+	}
 	else
+	{
+		str = (char*)va_arg(args, char*);
+		if (!str)
+			str = ft_strdup("(null)");
+		i = ft_strlen(str);
+		*fs = str;
+	}
+	if (info->precision == -1 || info->precision > i)
+		info->precision = i;
+	return (info->precision);
+}
+
+int			parse_s(t_info *info, va_list args)
+{
+	char		*str;
+	wchar_t		*wstr;
+	int			i;
+
+	i = get_and_save(info, &wstr, &str, args);
+	if (!flag_zero(info->flag_minus, info->flag_zero, info->min_size, i))
 		return (-1);
+	if (!flag_space(info->flag_minus, info->flag_zero, info->min_size, i))
+		return (-1);
+	if (info->lenght_modifs[0] == 'l' && info->lenght_modifs[1] == '\0')
+	{
+		i = 0;
+		while (wstr && wstr[i] && i < info->precision)
+			if (!addunicode((int)wstr[i++]))
+				return (-1);
+	}
+	else
+	{
+		if (!str || !addchars(str, info->precision))
+			return (-1);
+	}
+	if (info->flag_minus && (info->min_size > i))
+		if (!flag_space(0, 0, info->min_size, i))
+			return (-1);
+	return (1);
 }
