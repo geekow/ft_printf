56f0254bec6f06e15193b4576fdac078ab6cd686
Jean Jacobi <jjacobi@student.42.fr>
2017-01-30 17:56:01 +0100

    Fix bug big s with null value

--- srcs/parse_s.c
+++ srcs/parse_s.c
@@ -6,7 +6,7 @@
 /*   By: jjacobi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/11 01:43:06 by jjacobi           #+#    #+#             */
-/*   Updated: 2017/01/29 17:00:12 by jjacobi          ###   ########.fr       */
+/*   Updated: 2017/01/30 17:54:03 by jjacobi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 #include "libft.h"
 #include <wchar.h>
 
-static int	flag_zero(char flag_minus, char flag_zero, int min_size, int i)
+static int	flag_zero_spc(char flag_minus, char flag_zero, int min_size, int i)
 {
 	if (!flag_minus && flag_zero && ((min_size - i) > 0))
-		return (addchar('0', min_size - i));
+		if (-1 == addchar('0', min_size - i))
+			return (-1);
+	if (!flag_minus && !flag_zero && ((min_size - i) > 0))
+		if (-1 == addchar(' ', min_size - i))
+			return (-1);
 	return (1);
 }
 
@@ -39,8 +43,8 @@ static int	get_and_save(t_info *info, wchar_t **fws, char **fs, va_list args)
 	{
 		wstr = (wchar_t*)va_arg(args, wchar_t*);
 		if (!wstr)
-			wstr = (wchar_t*)ft_strdup("(null)");
-		while (wstr[i])
+			i = 6;
+		while (wstr && wstr[i])
 			i = i + 1;
 		*fws = wstr;
 	}
@@ -64,13 +68,13 @@ int			parse_s(t_info *info, va_list args)
 	int			i;
 
 	i = get_and_save(info, &wstr, &str, args);
-	if (!flag_zero(info->flag_minus, info->flag_zero, info->min_size, i))
-		return (-1);
-	if (!flag_space(info->flag_minus, info->flag_zero, info->min_size, i))
+	if (!flag_zero_spc(info->flag_minus, info->flag_zero, info->min_size, i))
 		return (-1);
 	if (info->lenght_modifs[0] == 'l' && info->lenght_modifs[1] == '\0')
 	{
 		i = 0;
+		if (!wstr && !addchars("(null)", info->precision))
+			return (-1);
 		while (wstr && wstr[i] && i < info->precision)
 			if (!addunicode((int)wstr[i++]))
 				return (-1);
