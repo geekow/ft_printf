8515df14306b64f751e9bc39e39eeff33d85a79e
Jean Jacobi <jjacobi@student.42.fr>
2017-02-01 19:24:49 +0100

    Null case fixed

--- srcs/parse_s.c
+++ srcs/parse_s.c
@@ -6,7 +6,7 @@
 /*   By: jjacobi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/11 01:43:06 by jjacobi           #+#    #+#             */
-/*   Updated: 2017/01/31 07:25:45 by jjacobi          ###   ########.fr       */
+/*   Updated: 2017/02/01 19:22:54 by jjacobi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ static int	get_and_save(t_info *info, wchar_t **fws, char **fs, va_list args)
 	if (info->lenght_modifs[0] == 'l' && info->lenght_modifs[1] == '\0')
 	{
 		wstr = (wchar_t*)va_arg(args, wchar_t*);
-		if (!wstr)
+		if (!(*fs = NULL) && !wstr)
 			i = 6;
 		else
 			i = calc_unicode_size(wstr, info->precision);
@@ -80,9 +80,9 @@ static int	get_and_save(t_info *info, wchar_t **fws, char **fs, va_list args)
 	else
 	{
 		str = (char*)va_arg(args, char*);
-		if (!str)
-			str = ft_strdup("(null)");
-		i = ft_strlen(str);
+		if (!(*fws = NULL) && str)
+			i = ft_strlen(str);
+		i = (!str) ? 6 : i;
 		*fs = str;
 	}
 	if (info->precision == -1 || info->precision > i)
@@ -100,17 +100,15 @@ int			parse_s(t_info *info, va_list args)
 	i[2] = i[0];
 	if (!flag_zero_spc(info->flag_minus, info->flag_zero, info->min_size, i[0]))
 		return (-1);
-	if (info->lenght_modifs[0] == 'l' && info->lenght_modifs[1] == '\0')
-	{
-		if (!(i[1] = 0) && !wstr && !addchars("(null)", info->precision))
-			return (-1);
+	if (!(i[1] = 0) && !wstr && !str && !addchars("(null)", info->precision))
+		return (-1);
+	if (wstr)
 		while (i[0] && wstr && wstr[i[1]] && i[1] < info->precision)
 			if ((i[3] = addunicode((int)wstr[i[1]++])) && i[3] == -1)
 				return (-1);
 			else
 				i[0] -= i[3];
-	}
-	else if (!str || !addchars(str, info->precision))
+	else if (str && !addchars(str, info->precision))
 		return (-1);
 	if (info->flag_minus && (info->min_size > i[2]))
 		if (!flag_space(0, 0, info->min_size, i[2]))
