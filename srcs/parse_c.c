6140fd5555f91f5955c50b83b1bf1a757f73f3c6
Jean JACOBI <jjacobi@student.42.fr>
2017-02-09 22:59:35 -0800

    Trying something

--- srcs/parse_c.c
+++ srcs/parse_c.c
@@ -6,7 +6,7 @@
 /*   By: jjacobi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/11 01:41:12 by jjacobi           #+#    #+#             */
-/*   Updated: 2017/02/05 00:58:34 by jjacobi          ###   ########.fr       */
+/*   Updated: 2017/02/09 22:51:25 by jjacobi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ int			parse_c(t_info *info, va_list args)
 		return (-1);
 	if (!flag_space(info->flag_minus, info->flag_zero, info->min_size, size))
 		return (-1);
-	if (info->lenght_modifs[0] == 'l' && info->lenght_modifs[1] == '\0')
+	if (info->lenght_modifs[0] == 'l' && info->lenght_modifs[1] == '\0' && MB_CUR_MAX > 1)
 	{
 		if (wc < 0 || wc > 1114111 || !addunicode((wchar_t)wc))
 			return (-1);
